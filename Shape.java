403d099105bc674e39349f67df444e23ecd75e09
B1gN1ck7 <159031791+B1gN1ck7@users.noreply.github.com>
2024-03-24 21:18:24 -0400

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -9,13 +9,15 @@ public abstract class Shape {
     protected int width;
     protected int height;
     protected Color color;
+    protected boolean fill;
 
-    public Shape(int x, int y, int width, int height, Color color) {
+    public Shape(int x, int y, int width, int height, Color color, boolean fill) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.color = color;
+        this.fill = fill;
     }
 
     public int getX() {return x;}
@@ -23,11 +25,13 @@ public abstract class Shape {
     public int getWidth() {return width;}
     public int getHeight() {return height;}
     public Color getColor() {return color;}
+    public boolean getFill() {return fill;}
     public void setX(int x) {this.x = x;}
     public void setY(int y) {this.y = y;}
     public void setwidth(int width) {this.width = width;}
     public void setheight(int height) {this.height = height;}
     public void setColor(Color color) {this.color = color;}
+    public void setFill(boolean fill) {this.fill = fill;}
 
     public abstract void draw(Graphics g);
 }
