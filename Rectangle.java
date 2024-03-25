16234426cb77381fcfb0d0ab74266ce7528e2d1b
B1gN1ck7 <159031791+B1gN1ck7@users.noreply.github.com>
2024-03-24 21:18:01 -0400

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -4,13 +4,18 @@ import java.awt.*;
 
 public class Rectangle extends Shape {
 
-    public Rectangle(int x, int y, int width, int height, Color color) {
-        super(x, y, width, height, color);
+    public Rectangle(int x, int y, int width, int height, Color color, boolean fill) {
+        super(x, y, width, height, color, fill);
     }
 
     @Override
     public void draw(Graphics g) {
-        g.setColor(color);
-        g.fillRect(x,y,width,height);
+        if(fill) {
+            g.setColor(color);
+            g.fillRect(x,y,width,height); 
+        } else {
+            g.setColor(color);
+            g.drawRect(x,y,width,height);
+        }
     }
 }
