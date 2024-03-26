571d233dd1ecf13df6f8536ce293d028daa58f23
B1gN1ck7 <159031791+B1gN1ck7@users.noreply.github.com>
2024-03-26 18:56:14 -0400

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,37 +1,18 @@
-// Shape.java -- can be extended as self-drawing rectangle, oval, etc.
-
 import java.awt.*;
 
-public abstract class Shape {
+public abstract class Shape extends Drawing {
 
-    protected int x;
-    protected int y;
     protected int width;
     protected int height;
-    protected Color color;
     protected boolean fill;
 
-    public Shape(int x, int y, int width, int height, Color color, boolean fill) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.color = color;
+    public Shape(int x, int y, Color color, boolean fill) {
+        super(x, y, color);
         this.fill = fill;
     }
 
-    public int getX() {return x;}
-    public int getY() {return y;}
-    public int getWidth() {return width;}
-    public int getHeight() {return height;}
-    public Color getColor() {return color;}
-    public boolean getFill() {return fill;}
-    public void setX(int x) {this.x = x;}
-    public void setY(int y) {this.y = y;}
-    public void setwidth(int width) {this.width = width;}
-    public void setheight(int height) {this.height = height;}
-    public void setColor(Color color) {this.color = color;}
-    public void setFill(boolean fill) {this.fill = fill;}
+    public boolean getFill() { return fill; }
+    public void setFill(boolean fill) { this.fill = fill; }
 
     public abstract void draw(Graphics g);
 }
