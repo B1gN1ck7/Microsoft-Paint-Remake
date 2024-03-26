ff2ba5ef580f7a3a251b8539b4189b551163fbaa
B1gN1ck7 <159031791+B1gN1ck7@users.noreply.github.com>
2024-03-26 18:57:17 -0400

    Add files via upload

---
+++ Drawing.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+public abstract class Drawing {
+    protected int x, y;
+    protected Color color;
+
+    public Drawing(int x, int y, Color color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public Color getColor() { return color; }
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+    public void setColor(Color color) { this.color = color; }
+
+    public abstract void draw(Graphics g);
+}
