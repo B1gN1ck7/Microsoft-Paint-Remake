1cb437f7b4166941d384832f79eb5a41be8b511c
Gibson-Adema <145415072+Gibson-Adema@users.noreply.github.com>
2024-03-27 22:07:52 -0400

    Update DashedLine.java

--- DashedLine.java
+++ DashedLine.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 
 public class DashedLine extends Line{
-    public DashedLine(int X, int Y, int currentX, int currentY, Color color){
-        super(X,Y,color);
+    public DashedLine(int X, int Y, int currentX, int currentY, int font, Color color){
+        super(X,Y,color, font);
         this.currentX = currentX;
         this.currentY = currentY;
     }
@@ -10,10 +10,11 @@ public class DashedLine extends Line{
     public void draw(Graphics g) {
         
             g.setColor(color);
-            Graphics2D g2d = (Graphics2D) g;
-            Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL,
+            Graphics2D g2d = (Graphics2D) g.create();
+            Stroke dashed = new BasicStroke(font, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL,
                                   0, new float[]{9}, 0);
             g2d.setStroke(dashed);
            g2d.drawLine(x, y, currentX, currentY);
+           g2d.dispose();
     }
 }
