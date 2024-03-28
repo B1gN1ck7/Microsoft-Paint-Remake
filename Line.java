298043651d59c49c5f0ba05329e9897d677e00b9
Gibson-Adema <145415072+Gibson-Adema@users.noreply.github.com>
2024-03-27 22:07:19 -0400

    Update Line.java

--- Line.java
+++ Line.java
@@ -2,11 +2,11 @@ import java.awt.*;
 
 public abstract class Line extends Drawing{
 
-
-    protected int startX, startY, currentX, currentY;
-    public Line(int X, int Y, Color color){
-        super(X,Y, color);
     
+    protected int startX, startY, currentX, currentY, font;
+    public Line(int X, int Y, Color color, int font){
+        super(X,Y, color);
+    this.font = font;
     }
 public abstract void draw(Graphics g);
 
