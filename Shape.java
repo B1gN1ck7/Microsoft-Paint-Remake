787bb0b03d65bea2ddc45c743c816716973e587b
B1gN1ck7 <159031791+B1gN1ck7@users.noreply.github.com>
2024-04-03 11:26:41 -0400

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,9 +1,24 @@
+/**
+ * This class extends drawing and further classifies the Drawing class.
+ * @author Gibson Adema, Nick Wiley
+ * @version 1.1
+ * "We did not copy code from anything or anyone other than the CIS-172 textbook. We did not use AI to aid in the making of our code."
+ */
 import java.awt.*;
 
 public abstract class Shape extends Drawing {
-
+    /**
+     * fill is what determines whether or not to draw or fill the shapes. 
+     */
     protected boolean fill;
 
+    /**
+     * Constructor for Shape thatallows for better organization and more distinct constructor parameters.
+     * @param x location of the shape
+     * @param y location of the shape
+     * @param color color of the shape
+     * @param fill fill or draw the shape. 
+     */
     public Shape(int x, int y, Color color, boolean fill) {
         super(x, y, color);
         this.fill = fill;
@@ -12,5 +27,9 @@ public abstract class Shape extends Drawing {
     public boolean getFill() { return fill; }
     public void setFill(boolean fill) { this.fill = fill; }
 
+    /**
+     * Abstract class of draw that will be implemented later into code. 
+     * @param g is what makes the drawing possible on the canvas.
+     */
     public abstract void draw(Graphics g);
 }
