aa715fe75defcc7e5ef29c14324bfd6d20d70375
Gibson-Adema <145415072+Gibson-Adema@users.noreply.github.com>
2024-04-03 08:10:46 -0400

    Update Pencil.java

--- Pencil.java
+++ Pencil.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 public class Pencil extends Drawing {
 
     private ArrayList<Point> pencilPoints;
-    private static ArrayList<ArrayList<Point>> pencilLines;
     int font;
 
     public Pencil(ArrayList<Point> pencilPoints, ArrayList<ArrayList<Point>> pencilLinesy, int font, Color color){
         super(color);
         this.font = font;
         this.pencilPoints = pencilPoints;
-        pencilLines = pencilLinesy;
+
     }
 
     public void draw(Graphics g){
@@ -19,12 +18,11 @@ public class Pencil extends Drawing {
         Graphics2D g2d = (Graphics2D) g.create();
         g2d.setStroke(new BasicStroke(font));
         
-        for(ArrayList<Point> thePoint : pencilLines){
-       for (int i = 1; i < thePoint.size(); i++) {
-        Point p1 = thePoint.get(i - 1);
-        Point p2 = thePoint.get(i);
+       for (int i = 1; i < pencilPoints.size(); i++) {
+        Point p1 = pencilPoints.get(i - 1);
+        Point p2 = pencilPoints.get(i);
         g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
-    }}
+    }
 
 
     }
@@ -33,7 +31,5 @@ public class Pencil extends Drawing {
 pencilPoints.add(p);
     }
 
-    public static void setPencilLines(ArrayList<ArrayList<Point>> pencilLinesy){
-            pencilLines = pencilLinesy;
-    }
+    
 }
