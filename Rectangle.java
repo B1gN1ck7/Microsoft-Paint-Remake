4caebc0bfedf053f3b9dc409857bdf407cb75129
B1gN1ck7 <159031791+B1gN1ck7@users.noreply.github.com>
2024-04-03 11:26:05 -0400

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,17 +1,40 @@
-// Rectangle.java -- draws a rectangle
-
+/**
+ * This class is what paints the rectangle and is the constructor that holds the objects to paint the rectangles.
+ * @author Gibson Adema, Nick Wiley
+ * @version 1.1
+ * "We did not copy code from anything or anyone other than the CIS-172 textbook. We did not use AI to aid in the making of our code."
+ */
 import java.awt.*;
 
 public class Rectangle extends Shape {
 
+    /**
+     * width of the rectangle
+     */
     private int width;
+    /**
+     * height of the rectangle
+     */
     private int height;
+    /**
+     * Constructor for the Rectangle class.
+     * @param x sets the location  
+     * @param y sets the location
+     * @param width sets the width
+     * @param height sets the height
+     * @param color sets the color 
+     * @param fill determines whether to draw or fill the rectangle
+     */
     public Rectangle(int x, int y, int width, int height, Color color, boolean fill) {
         super(x, y, color, fill);
         this.width = width;
         this.height = height;
     }
 
+    /**
+     * This is what draws the rectangle
+     * @param g is what makes the drawing on the canvas work
+     */
     @Override
     public void draw(Graphics g) {
         if(fill) {
