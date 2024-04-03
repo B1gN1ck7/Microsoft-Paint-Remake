1edd2f83f406841127fdae40dd021b86033f5532
B1gN1ck7 <159031791+B1gN1ck7@users.noreply.github.com>
2024-04-03 11:27:14 -0400

    Update DrawingInterface.java

--- DrawingInterface.java
+++ DrawingInterface.java
@@ -1,3 +1,9 @@
+/**
+ * This is the Interface for the drawing class and hold all of he stuff that the drawing class needs to implement. 
+ * @author Gibson Adema, Nick Wiley
+ * @version 1.1
+ * "We did not copy code from anything or anyone other than the CIS-172 textbook. We did not use AI to aid in the making of our code."
+ */
 import java.awt.*;
 
 public interface DrawingInterface {
@@ -8,5 +14,9 @@ public interface DrawingInterface {
     void setY(int y);
     void setColor(Color color);
 
+    /**
+     * This is the draw fuction that will draw on the canvas. Will be coded in in later classes
+     * @param g is what will make th drawing on a canvas availble. 
+     */
     void draw(Graphics g);
 }
