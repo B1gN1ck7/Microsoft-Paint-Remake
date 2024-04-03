663c64368a3374c5798edba56b2d99806f59415d
B1gN1ck7 <159031791+B1gN1ck7@users.noreply.github.com>
2024-04-03 11:24:27 -0400

    Update SolidLine.java

--- SolidLine.java
+++ SolidLine.java
@@ -1,7 +1,12 @@
+/**
+ * DESCRIPTION OF THE CLASS
+ * @author Gibson Adema, Nick Wiley
+ * @version 1.1
+ * "We did not copy code from anything or anyone other than the CIS-172 textbook. We did not use AI to aid in the making of our code."
+ */
 import java.awt.*;
 
 public class SolidLine extends Line{
-
 
     public SolidLine(int X, int Y, int currentX, int currentY, int font, Color color){
         super(X,Y,color,font);
@@ -10,10 +15,9 @@ public class SolidLine extends Line{
     }
     @Override
     public void draw(Graphics g) {
-        
-            g.setColor(color);
-            Graphics2D g2d = (Graphics2D) g.create();
-            g2d.setStroke(new BasicStroke(font));
-           g2d.drawLine(x, y, currentX, currentY);
+        g.setColor(color);
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setStroke(new BasicStroke(font));
+        g2d.drawLine(x, y, currentX, currentY);
     }
 }
