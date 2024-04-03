41b3b61ab8c2ff2f834ff5261728c6b1cd2ffee3
Gibson-Adema <145415072+Gibson-Adema@users.noreply.github.com>
2024-04-03 08:09:26 -0400

    Delete IntOutOfRangeException.java

--- IntOutOfRangeException.java
+++
@@ -1,5 +0,0 @@
-//gets the bound and says that its not there
-
-public class IntOutOfRangeException extends Exception {
-
-    public IntOutOfRangeException (int Bound) {super("The input value exceeds the bound " + Bound);} } 
