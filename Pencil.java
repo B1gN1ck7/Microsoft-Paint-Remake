5cdb70ff48b2630f08b494ea82f58f43cc312b82
B1gN1ck7 <159031791+B1gN1ck7@users.noreply.github.com>
2024-04-03 11:23:54 -0400

    Update Pencil.java

--- Pencil.java
+++ Pencil.java
@@ -1,8 +1,13 @@
+/**
+ * DESCRIPTION OF THE CLASS
+ * @author Gibson Adema, Nick Wiley
+ * @version 1.1
+ * "We did not copy code from anything or anyone other than the CIS-172 textbook. We did not use AI to aid in the making of our code."
+ */
 import java.awt.*;
 import java.util.ArrayList;
 
 public class Pencil extends Drawing {
-
     private ArrayList<Point> pencilPoints;
     int font;
 
@@ -10,7 +15,6 @@ public class Pencil extends Drawing {
         super(color);
         this.font = font;
         this.pencilPoints = pencilPoints;
-
     }
 
     public void draw(Graphics g){
@@ -18,18 +22,14 @@ public class Pencil extends Drawing {
         Graphics2D g2d = (Graphics2D) g.create();
         g2d.setStroke(new BasicStroke(font));
         
-       for (int i = 1; i < pencilPoints.size(); i++) {
-        Point p1 = pencilPoints.get(i - 1);
-        Point p2 = pencilPoints.get(i);
-        g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
-    }
-
-
+        for (int i = 1; i < pencilPoints.size(); i++) {
+            Point p1 = pencilPoints.get(i - 1);
+            Point p2 = pencilPoints.get(i);
+            g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
+        }   
     }
 
     public void addPoints(Point p){
-pencilPoints.add(p);
-    }
-
-    
+        pencilPoints.add(p);
+    }   
 }
