5566a1b92f191408718308ff37988616a777944b
B1gN1ck7 <159031791+B1gN1ck7@users.noreply.github.com>
2024-04-03 11:24:12 -0400

    Update DashedLine.java

--- DashedLine.java
+++ DashedLine.java
@@ -1,3 +1,9 @@
+/**
+ * DESCRIPTION OF THE CLASS
+ * @author Gibson Adema, Nick Wiley
+ * @version 1.1
+ * "We did not copy code from anything or anyone other than the CIS-172 textbook. We did not use AI to aid in the making of our code."
+ */
 import java.awt.*;
 
 public class DashedLine extends Line{
@@ -8,13 +14,11 @@ public class DashedLine extends Line{
     }
     @Override
     public void draw(Graphics g) {
-        
-            g.setColor(color);
-            Graphics2D g2d = (Graphics2D) g.create();
-            Stroke dashed = new BasicStroke(font, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL,
-                                  0, new float[]{9}, 0);
-            g2d.setStroke(dashed);
-           g2d.drawLine(x, y, currentX, currentY);
-           g2d.dispose();
+        g.setColor(color);
+        Graphics2D g2d = (Graphics2D) g.create();
+        Stroke dashed = new BasicStroke(font, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+        g2d.setStroke(dashed);
+        g2d.drawLine(x, y, currentX, currentY);
+        g2d.dispose();
     }
 }
