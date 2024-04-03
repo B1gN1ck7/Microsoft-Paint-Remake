f58cbcb86c4d385f44f04228da6370969e025065
B1gN1ck7 <159031791+B1gN1ck7@users.noreply.github.com>
2024-04-03 11:24:40 -0400

    Update Line.java

--- Line.java
+++ Line.java
@@ -1,13 +1,18 @@
+/**
+ * DESCRIPTION OF THE CLASS
+ * @author Gibson Adema, Nick Wiley
+ * @version 1.1
+ * "We did not copy code from anything or anyone other than the CIS-172 textbook. We did not use AI to aid in the making of our code."
+ */
 import java.awt.*;
 
 public abstract class Line extends Drawing{
-
-    
     protected int startX, startY, currentX, currentY, font;
+
     public Line(int X, int Y, Color color, int font){
         super(X,Y, color);
-    this.font = font;
+        this.font = font;
     }
-public abstract void draw(Graphics g);
 
+    public abstract void draw(Graphics g);
 }
